2b763573c895a4e17e9f24f7d96650f0af44b054
saimzaidi <saimzaidi719@gmail.com>
2024-07-23 10:12:38 -0700

    First commit

---
+++ Assignments/assignment-4.dart
@@ -0,0 +1,82 @@
+void main() {
+
+  //  PROGRAM 1 ( check if a given number is positive, negative, or zero.):
+
+  // int number= -16;
+  // if(number == 0) {
+  //   print("The number is equal to 0");
+  // }
+  // else if(number > 0) {
+  //   print("The number is Positive");
+  // }
+  // else{
+  //   print("The number is Negative");
+  // }
+
+  // PROGRAM 2 (check if a given number is even or odd.):
+
+  // int number = 3;
+  //  if((number % 2) == 0) {
+  //   print("The number is even");
+  //  }
+  //  else{
+  //   print("The number is Odd");
+  //  }
+
+  // PROGRAM 3 (CHECKIN LEAP YEAR): Leap year is divisible by 400, a year can also be leap if it is not divisible by 100, but divisible by 4.
+
+  // int year = 2001;
+  // if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+  //   print("The given year is a leap year");
+  // }
+  // else{
+  //   print("The given year is not a leap year");
+  // }
+
+  // PROGRAM 4 (Checking highest number):
+
+  // int n1 = 11;
+  // int n2 = 12;
+  // int n3 = 13;
+
+  // if(n1 > n2 && n1 > n3){
+  //   print("The highest number is $n1");
+  // }
+  // else if(n2 > n1 && n2 > n3){
+  //   print("The highest number is $n2");
+  // }
+  // else{
+  //   print("The highest number is $n3");
+  // }
+
+  // PROGRAM 5 (check if a given year is a century year (ends with 00).): if n is divisble by 100, so it is century year
+
+  // int numb = 88;
+  // if(numb % 100 == 0) {
+  //   print("$numb is a century year");
+  // }
+  // else{
+  //   print("$numb is not a century year");
+  // }
+
+  // PROGRAM 6 (check if a given number is divisible by 5 and 11.)
+
+  // int numb = 89;
+  // if(numb % 5 == 0 && numb % 11 == 0){
+  //   print("$numb is divisible by 5 & 11");
+  // }
+  // else{
+  //   print("$numb is not divisible by 5 & 11");
+  // }
+
+  // PROGRAM 7 (check if a given number is a multiple of 3 or 7.)
+
+  // int numb = 21;
+  // if (numb % 3 == 0 || numb % 7 == 0){
+  //   print("$numb is divisible by 3 and 7");
+  // }
+  // else{
+  //    print("$numb is not divisible by 3 and 7");
+  // }
+
+}
