91b84f6cda91145367d54a7d647ef8ca7478fd6f
saimzaidi <saimzaidi719@gmail.com>
2024-07-24 11:12:51 -0700

    Adding Assignment number 5

---
+++ Assignments/assignment-5.dart
@@ -0,0 +1,31 @@
+void main() {
+
+  // Q.1) Create a list of names and print all names using list:
+
+  // List<String> names = ["saim", "raza", "ali", "mohsin", "kazim"];
+  // print(names[0]);
+  // print(names[1]);
+  // print(names[2]);
+  // print(names[3]);
+  // print(names[4]);
+
+
+
+
+  // Q. 2) Create a list of Days and print only  Sunday:
+
+  // List<String> days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  // print(days[6]);
+
+
+
+
+  // Q. 3)  create a list  of name, class, roll no, grade, percentage. And print:
+
+  List data = ["Saim", 12, 72, "A+", "80%"];
+  print("The name of student is ${data[0]}");
+  print("The class of student is ${data[1]}");
+  print("The roll no of student is ${data[2]}");
+  print("The grade of student is ${data[3]}");
+  print("The percentage of student is ${data[4]}");
+}
