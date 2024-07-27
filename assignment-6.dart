fc4785cdafd81bff272e3a6c9267dbadb3cd0f0b
saimzaidi <saimzaidi719@gmail.com>
2024-07-27 02:40:35 -0700

    Uploading Assignment 6

---
+++ assignment-6.dart
@@ -0,0 +1,36 @@
+void main() {
+
+  // Q.1: Create a list of numbers & write a program to get the smallest & greatest number from a list.
+
+  // List<int> numbers = [10, 20, 30, 40, 50, 60, 70];
+  // numbers.sort();
+  // print("The smallest number in the list is ${numbers.first}");
+  // print("The largest number in the list is ${numbers.last}");
+
+  // Q.2: remove all false values from below list by using removeWhere or retainWhere property.
+
+  // List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+  // usersEligibility.removeWhere((ele) => ele != 'eligible');
+  // print(usersEligibility);
+
+  // Q.3 : Given a list of integers, write a dart code that returns the maximum value from the list.
+
+  // List<int> numbers = [22, 33, 19, 10, 100, 01];
+  // numbers.sort();
+  // print(numbers.last);
+
+  // Q.4: Write a Dart code that takes in a list of strings and prints a new list with the elements in reverse order. The original list should remain unchanged.
+
+  // List names = ["Saim", "Raza", "Zaidi", "Ali"];
+  // List newNames = names.reversed.toList();
+  // print("The names in Reverse order are : $newNames");
+  // print("The names in Original order are : $names");
+
+
+  // Q.5: Implement a Dart code that uses the where() method to filter out negative numbers from a list of integers. The program should take in the original list as a parameter and print a new list containing only the positive numbers.
+
+  // List<int> numbers = [-21, 32, 11, 109, 112, -22, 0, -909, 134];
+  // Iterable<int> positiveNum = numbers.where((num) => num >= 0);
+  // print(positiveNum.toList());
+
+}
