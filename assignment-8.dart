6c9364b729c9cd3a41b286f3ed9498440b47ad8d
saimzaidi <saimzaidi719@gmail.com>
2024-08-10 06:56:30 -0700

    Assignement 8

---
+++ assignment-8.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+void main() {
+  // if else practise : 
+
+  // print("Enter your age?");
+  // int userAge = int.parse(stdin.readLineSync()!); // ! sign shows that the value is 100% non null, and it is important because the function "int.parse()" requires a values and input can be null, that's why. If the value is null, there will be a error because of this line.
+
+  // print("Enter your gender?");
+  // String? userGender = stdin.readLineSync(); // ? declare the variable as nullable, means can hold both values null and non null. It is important bcz the function can return null value.
+
+  // if(userGender != null && userGender.isNotEmpty){
+
+  //   if(userAge >= 18 && userGender.toLowerCase() == "male"){
+  //     print("You are allowed to ride!");
+  //   }
+  //   else{
+  //     print("You are not allowed to ride");
+  //   }
+  // }
+
+
+
+
+
+
+
+
+  // list practise : 
+
+  // List<int> numbers  = [10, 20, 30, 40, 50];
+
+  // numbers.forEach((e) {
+  //   print(e);
+  // });
+
+  // for (int ele in numbers) {
+  //   print(ele);
+  // }
+
+  // for (var i = 0; i < numbers.length; i++) {
+  //   print(numbers[i]);
+  // }
+
+  // int i = 0;
+  // while (i < numbers.length) {
+  //   print(numbers[i]);
+  //   i++;
+  // }
+
+
+
+
+
+
+  // map practise : 
+
+  // Map map = {
+  //   "name" : "Saim", 
+  //   "cls" : 12, 
+  //   "age" : 17
+  // };
+
+  // map.forEach((key, val) {
+  //   print("Key is $key");
+  //   print("Value is $val");
+  // });
+
+  // for (var ele in map.entries) {
+  //   print("Key is ${ele.key}");
+  //   print("Value is ${ele.value}");
+  // }
+
+
+
+
+  
+
+  // loop practise is done above....
+
+}
