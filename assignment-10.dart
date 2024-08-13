bdac77daf721f2c4a25bea69bae8e5b72016f92f
saimzaidi <saimzaidi719@gmail.com>
2024-08-13 01:41:25 -0700

    Uploading assignment 10

---
+++ assignment-10.dart
@@ -0,0 +1,205 @@
+import 'dart:io';
+
+void main() {
+  // Q1. Write a program that prints the Fibonacci sequence up to a given number using a for loop:
+
+  // print("Enter any number?");
+  // int number = int.parse(stdin.readLineSync()!);
+
+  // int first = 0, second = 1;
+
+  // for (var i = 0; i < number; i++) {
+  //     print(first);
+  //     int nextNum = first + second;
+
+  //     first = second;
+  //     second = nextNum;
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Q2.  Implement a code that finds the largest element in a list using a for loop:
+
+  // List nums = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  // int largest = nums[0];
+
+  // for (var i = 1; i < nums.length; i++) {
+  //   if(nums[i] > largest) {
+  //     largest = nums[i];
+  //   }
+  // }
+
+  // print(largest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  // Q4. Write a program that takes a list of numbers as input and prints the numbers greater than 5 using a for loop and if-else condition.
+
+  // print("Enter any number?");
+  // int n1 = int.parse(stdin.readLineSync()!);
+
+  // print("Enter another number?");
+  // int n2 = int.parse(stdin.readLineSync()!);
+
+  // print("Enter one another number?");
+  // int n3 = int.parse(stdin.readLineSync()!);
+
+  // List<int> numbers = [n1, n2, n3];
+
+  // for (var i = 0; i < numbers.length; i++) {
+  //   if (numbers[i] > 5) {
+  //     print(numbers[i]);
+  //   }
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Q5.  Write a program that counts the number of vowels in a given string using a for loop and if-else condition.
+
+  // print("Enter any word?");
+  // String word = stdin.readLineSync()!.toLowerCase();
+  // int counter = 0;
+  // List vowels = ["a", "e" "i", "o", "u"];
+
+  // if (word != null) {
+  //   for (var i = 0; i < word.length; i++) {
+  //     if (word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u") {
+  //       counter++;
+  //     }
+  //   }
+  //   print("The vowels in the given words = ${counter}");
+  // }
+
+
+
+
+
+
+
+
+
+
+  // Q6.Write a Dart program that uses a while loop to calculate and print the factorial of a given number n:
+
+  // print("Enter any number??");
+  // int n = int.parse(stdin.readLineSync()!);
+  // int fac = 1;
+
+  // for (var i = n; i >= 1; i--) {
+  //   fac *= i;
+  // }
+
+  // print(fac);
+
+
+
+
+
+
+
+
+
+  // Q7. Write a Dart program that uses a for loop to print the numbers from 1 to 10, each on a new line. Modify the program to print the numbers in reverse order, from 10 to 1.
+
+  // for (var i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  // for (var i = 19; i >= 1; i--) {
+  //   print(i);
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Q8.Write a Dart program that uses a for loop to calculate and print the sum of all even numbers from 1 to 20. The program should output the total sum.
+
+  // int sum = 0;
+  // for (var i = 1; i <= 20; i++) {
+  //   if(i % 2 == 0){
+  //     sum += i;
+  //   }
+  // }
+  // print("The sum of Even numbers from 1 to 20 are : ${sum}");
+
+
+
+
+
+
+
+
+  // Q9. Write a Dart program that uses a for loop to print the first 5 multiples of 3.
+
+  // for (var i = 1; i <= 5; i++) {
+  //   print(3 * i);
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Q10.Write a Dart program that uses a while loop to print numbers from 1 to 3. Each number should be printed on a new line.
+
+  // int i = 1;
+  // while(i <= 3){
+  //   print(i);
+  //   i++;
+  // }
+
+}
